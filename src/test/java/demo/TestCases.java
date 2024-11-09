2e1a866bda7e87d739f57bcb616fe2202730e2ec
Aakash20071994 <167666601+Aakash20071994@users.noreply.github.com>
2024-11-09 20:03:25 +0530

    ME_QA_XSCRAPE_DATA_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,6 +1,7 @@
 package demo;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -10,9 +11,20 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.logging.Level;
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
+//import dev.failsafe.internal.util.Assert;
 
 public class TestCases {
     ChromeDriver driver;
@@ -47,6 +59,87 @@ public class TestCases {
         driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
+    }
+    @Test
+    public void testCase01() throws InterruptedException{
+        driver.get("https://www.scrapethissite.com/pages/");
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.scrapethissite.com/pages/", "Website URL does not match.");
+        System.out.println("Verified URL:https://www.scrapethissite.com/pages/");
+
+        WebElement hockeyTeamsElement=driver.findElement(By.xpath("//a[contains(text(),'Hockey Teams: Forms, Searching and Pagination')]"));
+        Wrappers.clickOnElement(hockeyTeamsElement, driver);
+        
+        //Initialize and declare a hashmap arraylist called datalist
+        ArrayList<HashMap<String, Object>> dataList=new ArrayList<>();
+
+        //Locate page no 1
+        WebElement clickOnPage=driver.findElement(By.xpath("(//ul[@class='pagination']//li/a)[1]"));
+        Wrappers.clickOnElement(clickOnPage, driver);
+
+        //Iterate through 4 pages
+        for(int page=1;page<=4;page++){
+            List<WebElement> rows=driver.findElements(By.xpath("//tr[@class='team']"));
+            for(WebElement row:rows){
+                //Extract data from each row
+                //Get Text from Teamname locator
+                String teamName=row.findElement(By.xpath("./td[@class='name']")).getText();
+                int year = Integer.parseInt(row.findElement(By.xpath("./td[@class='year']")).getText());
+                double winPercentage = Double.parseDouble(row.findElement(By.xpath("./td[contains(@class,'pct')]")).getText());
+                long epoch=System.currentTimeMillis()/1000;
+                String epochTime=String.valueOf(epoch);
+
+                //Check if win percentage is less than 40%
+                if(winPercentage<0.4){
+                    HashMap<String,Object> dataMap=new HashMap<>();
+                    dataMap.put("epochTime", epochTime);
+                    dataMap.put("teamName", teamName);
+                    dataMap.put("year", year);
+                    dataMap.put("winPercentage", winPercentage);
+                    dataList.add(dataMap);
+                }
+
+            }
+            if(page<4){
+                WebElement nextPagWebElement=driver.findElement(By.xpath("//a[@aria-label='Next']"));
+                nextPagWebElement.click();
+                Thread.sleep(5000);
+            }
+        }
+        for(HashMap<String,Object> data : dataList){
+            System.out.println("Epoch time of Scrape:"+data.get("epochTime")+",TeamName:"+data.get("teamName")+",Year:"+data.get("year")+",Win%:"+data.get("winPercentage"));
+
+        }
+        //Store the hashMap data in a json file
+        ObjectMapper mapper = new ObjectMapper();
+        try{
+            String userDir=System.getProperty("user.dir");
+            File jsonFile=new File(userDir+"/src/test/resources/hockey-team-data.json");
+            mapper.writeValue(jsonFile, dataList);
+            System.out.println("JSON data written to:"+jsonFile.getAbsolutePath());
+            Assert.assertTrue(jsonFile.length()!=0);
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        
+
+
+    }
+
+    @Test
+    public void testCase02(){
+        driver.get("https://www.scrapethissite.com/pages/");
+        
+        WebElement oscarWiningFilms=driver.findElement(By.xpath("//a[contains(text(),'Oscar')]"));
+        Wrappers.clickOnElement(oscarWiningFilms, driver);
+
+        Wrappers.scrape("2015", driver);
+        Wrappers.scrape("2014", driver);
+        Wrappers.scrape("2013", driver);
+        Wrappers.scrape("2012", driver);
+        Wrappers.scrape("2011", driver);
+        Wrappers.scrape("2010", driver);
+
     }
 
     @AfterTest
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -3,17 +3,112 @@ package demo.wrappers;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.idealized.Javascript;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
 public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+    public static boolean clickOnElement(WebElement element, WebDriver driver) {
+        if (element.isDisplayed()) {
+            try {
+                JavascriptExecutor js = (JavascriptExecutor) driver;
+                js.executeScript("arguments[0].scrollIntoView(true)", element);
+                element.click();
+                Thread.sleep(2000);
+                return true;
+            } catch (Exception e) {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public static boolean enterText(WebElement element, WebDriver driver, String text) {
+        try {
+            element.click();
+            element.clear();
+            element.sendKeys(text);
+            Thread.sleep(1000);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static void scrape(String year,WebDriver driver){
+
+        try{
+            WebElement yearLink=driver.findElement(By.id(year));
+            String yearLinkText=yearLink.getText();
+            Wrappers.clickOnElement(yearLink, driver);
+
+            WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(30));
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//table[@class='table']")));
+        
+        ArrayList<HashMap<String,String>> movieList=new ArrayList<>();
+        List<WebElement> filmRows=driver.findElements(By.xpath("//tr[contains(@class,'film')]"));
+        int count =0;
+        for(WebElement filmRow:filmRows){
+            String filmTitle=filmRow.findElement(By.xpath("./td[contains(@class,'title')]")).getText();
+            String nomination=filmRow.findElement(By.xpath("./td[contains(@class,'film-nominations')]")).getText();
+            String awards=filmRow.findElement(By.xpath("./td[contains(@class,'awards')]")).getText();
+            boolean isWinnerFlag=count==0;
+            String isWinner=String.valueOf(isWinnerFlag);
+
+            long epoch=System.currentTimeMillis()/1000;
+            String epochTime=String.valueOf(epoch);
+
+            HashMap<String,String> movieMap=new HashMap<>();
+            movieMap.put("epochTime",epochTime);
+            movieMap.put("year",yearLinkText);
+            movieMap.put("title",filmTitle);
+            movieMap.put("nomination",nomination);
+            movieMap.put("awards", awards);
+            movieMap.put("isWinner", isWinner);
+
+            movieList.add(movieMap);
+            count++;
+
+
+        }
+        for(HashMap<String,String> movieData:movieList){
+            System.out.println("Epoch time:"+ movieData.get("epochTime")+", Year:"+movieData.get("year")+", Film title:"+movieData.get("title")+", Nomonation:"+movieData.get("nomination")+",Awards:"+movieData.get("awards")+", Best Picture:"+movieData.get("isWinner"));
+        }
+         ObjectMapper mapper = new ObjectMapper();
+        try{
+            String userDir=System.getProperty("user.dir");
+            File jsonFile=new File(userDir+"/src/test/resources/"+year+"-oscar-winner-data.json");
+            mapper.writeValue(jsonFile, movieList);
+            System.out.println("JSON data written to:"+jsonFile.getAbsolutePath());
+            Assert.assertTrue(jsonFile.length()!=0);
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    catch(Exception e){
+        System.out.println("Web Scrap for movies is failed..");
+        e.printStackTrace();
+    }
+    }
 }
